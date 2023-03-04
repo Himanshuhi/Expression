324923a122198167d333a73b10cdb0f4b5b387d4
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-04 21:11:53 +0530

    Add files via upload

---
+++ expression.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAX_EXPR_LENGTH 256
+
+double evaluate(char *expr) {
+    char *p;
+    double left_operand, right_operand;
+    char operator;
+    
+    // Evaluate expressions with parentheses first
+    while ((p = strchr(expr, '(')) != NULL) {
+        char *q = p + 1;
+        int paren_count = 1;
+        while (*q != '\0') {
+            if (*q == '(') {
+                paren_count++;
+            } else if (*q == ')') {
+                paren_count--;
+                if (paren_count == 0) {
+                    *q = '\0';
+                    double result = evaluate(p + 1);
+                    sprintf(p, "%.3f%s", result, q + 1);
+                    break;
+                }
+            }
+            q++;
+        }
+    }
+    
+    // Evaluate expressions with exponentiation next
+    p = expr;
+    while (*p != '\0') {
+        if (*p == '^') {
+            operator = *p;
+            *p = '\0';
+            left_operand = evaluate(expr);
+            right_operand = evaluate(p + 1);
+            return pow(left_operand, right_operand);
+        }
+        p++;
+    }
+    
+    // Evaluate expressions with multiplication and division next
+    p = expr;
+    while (*p != '\0') {
+        if (*p == '*' || *p == '/') {
+            operator = *p;
+            *p = '\0';
+            left_operand = evaluate(expr);
+            right_operand = evaluate(p + 1);
+            switch (operator) {
+                case '*':
+                    return left_operand * right_operand;
+                case '/':
+                    return left_operand / right_operand;
+            }
+        }
+        p++;
+    }
+    
+    // Evaluate expressions with addition and subtraction last
+    p = expr;
+    while (*p != '\0') {
+        if (*p == '+' || *p == '-') {
+            operator = *p;
+            *p = '\0';
+            left_operand = evaluate(expr);
+            right_operand = evaluate(p + 1);
+            switch (operator) {
+                case '+':
+                    return left_operand + right_operand;
+                case '-':
+                    return left_operand - right_operand;
+            }
+        }
+        p++;
+    }
+    
+    // If no operators found, return the numeric value of the expression
+    return atof(expr);
+}
+
+int main() {
+    char expr[MAX_EXPR_LENGTH];
+    printf("Enter a math expression: ");
+    fgets(expr, MAX_EXPR_LENGTH, stdin);
+    expr[strcspn(expr, "\n")] = '\0'; // Remove trailing newline character
+    printf("Result: %.3f\n", evaluate(expr));
+    return 0;
+}
